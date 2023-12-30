79c9ebf2da53853c2a653195a11fff31bbd12e4d
Mahdi Lamb <mahdilamb@gmail.com>
2023-12-30 13:39:08 +0000

    Add other activities - update routes for other activities

--- app/activities/[activity]/years/[year]/page.tsx
+++ app/activities/[activity]/years/[year]/page.tsx
@@ -6,16 +6,17 @@ import {
 } from "@/app/strava-service/service";
 import { useEffect, useState } from "react";
 import { Map } from "@/app/components/map";
-import { useParams } from "next/navigation";
+import { notFound, useParams } from "next/navigation";
 import { yearToSpan } from "@/app/dates";
 import { getToken } from "@/app/tokens";
 import { getActivities, initDB, syncDB } from "@/app/databases";
+import { ActivityType } from "@/app/strava";
 
 export default function Year() {
   const [auth, setAuth] = useState(null);
   const [stravaAuthUrl, setStravaAuthUrl] = useState<string>();
   const [activities, setActivities] = useState<SummaryActivityDecoded[]>([]);
-  const params = useParams<{ year: string }>();
+  const params = useParams<{ year: string; activity: ActivityType }>();
   useEffect(() => {
     const checkAuth = async () => {
       var potentialToken;
@@ -30,11 +31,18 @@ export default function Year() {
   useEffect(() => {
     if (auth !== null) {
       const getDB = async () => {
+        const allActivities = Object.keys(ActivityType);
+        const activity = allActivities.find(
+          (activity) => (params?.activity as string) === activity.toLowerCase(),
+        );
+        if (activity === undefined) {
+          return notFound();
+        }
         const db = await syncDB(await initDB(), auth);
         setActivities(
           await getActivities(
             db,
-            "Run",
+            activity as ActivityType,
             yearToSpan(parseInt(params?.year as string)),
           ),
         );
