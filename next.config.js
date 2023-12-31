0201fc3728f445c7ab0a3b39bb6fe19ab9610c8d
Mahdi Lamb <mahdilamb@gmail.com>
2023-12-31 09:08:26 +0000

    Add dockerfile

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = { reactStrictMode: false};
+const nextConfig = { reactStrictMode: false, output: 'standalone', };
 
 module.exports = nextConfig;
