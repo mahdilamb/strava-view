182624d1752ba88dedcfac0e0ccfaf39aafebe85
Mahdi Lamb <mahdilamb@gmail.com>
2024-10-07 17:07:20 +0100

    Move from ts-node-dev to nodemon

--- app/api/ws/socket.ts
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from "next/server"
-
-export const socket =()=>{
-    return NextResponse.json('hi')
-}
--- server.ts
+++ server.ts
@@ -13,43 +13,52 @@ const wss = new WebSocketServer({ noServer: true });
 const app = next({ dev, port });
 const handle = app.getRequestHandler();
 
-app.prepare().then(async () => {
-  const server = createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true);
-    handle(req, res, parsedUrl);
-  }).listen(port);
+app
+  .prepare()
+  .then(async () => {
+    const server = createServer((req, res) => {
+      try {
+        return handle(req, res);
+      } catch (err) {
+        console.error(req.url, err);
+      }
+    }).listen(port);
 
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? "development" : process.env.NODE_ENV
-    }`,
-  );
-  await subscribeToWebhook();
-
-  wss.on("connection", (ws) => {
-    ws.on("message", (message: RawData) =>
-      socketClients.registerClient(ws, JSON.parse(message.toString()).id),
+    console.log(
+      `> Server listening at http://localhost:${port} as ${
+        dev ? "development" : process.env.NODE_ENV
+      }`,
     );
+    await subscribeToWebhook();
 
-    ws.on("close", () =>
-      console.log(
-        `Client disconnected (${socketClients.unregisterClient(ws)
-          ?.athleteId})`,
-      ),
-    );
+    wss.on("connection", (ws) => {
+      ws.on("message", (message: RawData) =>
+        socketClients.registerClient(ws, JSON.parse(message.toString()).id),
+      );
+
+      ws.on("close", () =>
+        console.log(
+          `Client disconnected (${socketClients.unregisterClient(ws)
+            ?.athleteId})`,
+        ),
+      );
+    });
+
+    server.on("upgrade", (req, socket, head) => {
+      const { pathname } = parse(req.url || "/", true);
+
+      if (pathname === "/_next/webpack-hmr") {
+        app.getUpgradeHandler()(req, socket, head);
+      }
+
+      if (pathname === WEBSOCKET_PATH) {
+        wss.handleUpgrade(req, socket, head, (ws) => {
+          wss.emit("connection", ws, req);
+        });
+      }
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
   });
-
-  server.on("upgrade", (req, socket, head) => {
-    const { pathname } = parse(req.url || "/", true);
-
-    if (pathname === "/_next/webpack-hmr") {
-      app.getUpgradeHandler()(req, socket, head);
-    }
-
-    if (pathname === WEBSOCKET_PATH) {
-      wss.handleUpgrade(req, socket, head, (ws) => {
-        wss.emit("connection", ws, req);
-      });
-    }
-  });
-});
