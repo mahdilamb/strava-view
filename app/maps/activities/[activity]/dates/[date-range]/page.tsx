e74c23884e22b989cbc2e034764a6327b3ceffa7
Mahdi Lamb <mahdilamb@gmail.com>
2024-10-12 11:14:44 +0100

    add first and last position (#27)
    * add first and last position

--- app/maps/activities/[activity]/dates/[date-range]/page.tsx
+++ app/maps/activities/[activity]/dates/[date-range]/page.tsx
@@ -6,11 +6,14 @@ import { useEffect, useRef } from "react";
 import { Polyline } from "react-leaflet";
 import { LatLngTuple } from "leaflet";
 import { useMapContext } from "@/app/maps/context";
+import { useSearchParams } from "next/navigation";
 
 
-
+type PointsView = 'start' | 'end' | null
 export default function StravaOverlay() {
   const { db, activityFilter, selectedDate } = useMapContext()
+  const params = useSearchParams()
+  const pointsView = params.get('points') as PointsView
   const positions = useRef<LatLngTuple[]>()
 
   useEffect(() => {
@@ -18,10 +21,15 @@ export default function StravaOverlay() {
       if (!db) {
         return
       }
-
       positions.current = Object.values(await getActivities(db, activityFilter, selectedDate)).filter(el => el.map?.summaryPolyline).map(el => el.map?.summaryPolyline)
     }
     updatePositions()
   }, [activityFilter, db, selectedDate])
+  if (pointsView === 'start') {
+    return positions.current && <Polyline positions={positions.current.map(pos => [pos[0], pos[0]])}></Polyline>
+  }
+  if (pointsView === 'end') {
+    return positions.current && <Polyline positions={positions.current.map(pos => [pos.at(-1), pos.at(-1)])}></Polyline>
+  }
   return positions.current && <Polyline positions={positions.current}></Polyline>
 }
